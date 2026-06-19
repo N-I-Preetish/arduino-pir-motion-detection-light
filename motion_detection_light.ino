e36886a2374ff3a5e57007c2a1fd5bf27bfffa92
N-I-Preetish <preetishni2008@gmail.com>
2026-06-19 14:43:38 +0530

    Added Arduino source code

---
+++ motion_detection_light.ino
@@ -0,0 +1,17 @@
+int pirPin = 2;
+int ledPin = 13;
+
+void setup() {
+  pinMode(pirPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  if (digitalRead(pirPin) == HIGH) {
+    digitalWrite(ledPin, HIGH);
+    delay(5000);
+  }
+  else {
+    digitalWrite(ledPin, LOW);
+  }
+}
